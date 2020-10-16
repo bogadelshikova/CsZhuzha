9eedbb504f27ebcae96bccde0e337f5dfd826bf8
student <student@C429U44>
2020-10-16 17:46:27 +0300

    zhuzha

--- praktika7/offTop/Risovat_na_Canvas/Properties/Resources.Designer.cs
+++ praktika7/offTop/Risovat_na_Canvas/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace WindowsFormsApp1.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _1 {
+            get {
+                object obj = ResourceManager.GetObject("1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap PRO_C__7 {
             get {
                 object obj = ResourceManager.GetObject("PRO_C#_7", resourceCulture);
